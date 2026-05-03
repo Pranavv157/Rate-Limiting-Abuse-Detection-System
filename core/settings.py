47061eb613a97088684a262bc9cab2d387ca25b5
Pranavv157 <pranavsshinde157@gmail.com>
2026-05-04 00:53:07 +0530

    Fixed internal settings issues

--- core/settings.py
+++ core/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--l32*_v#&s*2ya9&8xams3o+@ddyp8l(!nsz2yngdbvs&d)3%w'
+import os
+from dotenv import load_dotenv
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+load_dotenv()
 
-ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "True") == "True"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
